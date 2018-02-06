84b98bbe44b52e3f4250b173e08399976a1ff906
syun <517897442@qq.com>
2018-02-06 20:37:06 +0800

    安卓打包适配

--- android/app/src/main/java/com/goforeat_app/MainApplication.java
+++ android/app/src/main/java/com/goforeat_app/MainApplication.java
@@ -3,8 +3,8 @@ package com.goforeat_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.beefe.picker.PickerViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.beefe.picker.PickerViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new PickerViewPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new PickerViewPackage()
       );
     }
 
