f3ac0e1b8e7e9e839a0afa21ebe86ab99f697b04
syun <517897442@qq.com>
2018-07-16 09:02:13 +0800

    修改paysetting页面

---
+++ goforeat_app/app/styles/paysetting.style.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+import GLOBAL_PARAMS from '../utils/global_params';
+
+export default StyleSheet.create({
+  payLeftImage: {
+    width: 20,height: 20,
+    marginRight: 11.5,
+  },
+  payRightImage: {
+    width: 20,height: 20,
+    marginRight: 6
+  },
+  creditcardView: {
+    height: 44,
+    justifyContent: 'center',
+    marginLeft: 15,
+  },
+  creditcardText: {
+    color: '#666666',
+    fontSize: 14,
+  }
+})
--- goforeat_app/app/views/PaySettingView.js
+++ goforeat_app/app/views/PaySettingView.js
@@ -1,38 +1,81 @@
 import React, { PureComponent } from 'react';
-import {View} from 'react-native';
+import {View,Image,Text} from 'react-native';
 import {Container,Content} from 'native-base';
 //components
 import CommonItem from '../components/CommonItem';
 import CommonHeader from '../components/CommonHeader';
+import Divider from '../components/Divider';
 //utils
 import Colors from '../utils/Colors';
+//styles
+import PaySettingStyles from '../styles/paysetting.style';
 
-const PaySettingView = props => {
-  const _list_arr = [
-    {
-      content: '現金支付',isEnd: false,clickFunc: () => {}
-    },
-    {
-      content: 'apple pay',isEnd: false,clickFunc: () => {}
-    },
-    {
-      content: '信用卡支付',isEnd: true,clickFunc: () => {
-        props.navigation.navigate('Credit');
-      }
+const _checked = '../asset/checked.png';
+const _unchecked = '../asset/unchecked.png';
+const LIST_IMAGE = {
+  CASH: require('../asset/cash.png'),
+  APPLE_PAY: require('../asset/apple_pay.png'),
+  CREDIT_CARD: require('../asset/creditcard.png'),
+  WECHAT: require('../asset/wechat_pay.png'),
+  ALI: require('../asset/ali_pay.png'),
+};
+
+export default class PaySettingView extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkedName: 'cash'
     }
-  ]
-  return (
-    <Container>
-    <CommonHeader title="我的支付方式" canBack {...props}/>
-      <Content style={{backgroundColor:Colors.main_white}}>
-      <View>
-      {_list_arr.map((item,key) => (
-        <CommonItem key={key} content={item.content} isEnd={item.isEnd} clickFunc={item.clickFunc}/>
-      ))}
-      </View>
-    </Content>
-    </Container>
-  )
-}
+  }
 
-export default PaySettingView;
+  _checked(name) {
+    this.setState(
+      {checkedName: name}
+    )
+  }
+
+  _checkedImage(name) {
+    if(this.state.checkedName == name) {
+      return (<Image source={require(_checked)} style={PaySettingStyles.payRightImage} resizeMode="contain"/>)
+    }else{
+      return (<Image source={require(_unchecked)} style={PaySettingStyles.payRightImage} resizeMode="contain"/>)
+    }
+  }
+
+  _leftImage(image) {
+    return (<Image source={image} style={PaySettingStyles.payLeftImage} resizeMode="contain"/>)
+  }
+   
+  render() {
+    const _list_arr = [
+      {
+        content: '現金支付',hasLeftIcon: true,leftIcon:this._leftImage(LIST_IMAGE.CASH),rightIcon:this._checkedImage('cash'),clickFunc: () => this._checked('cash')
+      },
+      {
+        content: 'Apple Pay',hasLeftIcon: true,leftIcon:this._leftImage(LIST_IMAGE.APPLE_PAY),rightIcon:this._checkedImage('apple_pay'),clickFunc: () => this._checked('apple_pay')
+      },
+      {
+        content: 'WeChat Pay',hasLeftIcon: true,leftIcon:this._leftImage(LIST_IMAGE.WECHAT),rightIcon:this._checkedImage('wechat'),clickFunc: () => this._checked('wechat')
+      },
+      {
+        content: 'Ali Pay',hasLeftIcon: true,isEnd: true,leftIcon:this._leftImage(LIST_IMAGE.ALI),rightIcon:this._checkedImage('ali'),clickFunc: () => this._checked('ali')
+      }
+    ]
+    return (
+      <Container>
+      <CommonHeader title="我的支付方式" canBack {...this.props}/>
+        <Content style={{backgroundColor:Colors.main_white}}>
+        <View>
+        {_list_arr.map((item,key) => (
+          <CommonItem key={key} content={item.content} isEnd={item.isEnd} clickFunc={item.clickFunc}
+          hasLeftIcon={item.hasLeftIcon} leftIcon={item.leftIcon} rightIcon={item.rightIcon}/>
+        ))}
+        <View style={PaySettingStyles.creditcardView}><Text style={PaySettingStyles.creditcardText}>信用卡支付</Text></View>
+        <CommonItem content="設定您的信用卡" hasLeftIcon leftIcon={this._leftImage(LIST_IMAGE.CREDIT_CARD)}
+        clickFunc={() => this.props.navigation.navigate("Credit")}/>
+        </View>
+      </Content>
+      </Container>
+    )
+  }
+}
