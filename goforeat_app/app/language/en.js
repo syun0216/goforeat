021e7bd7e056f3d2000e9639efea762c9e8cd24c
syun <517897442@qq.com>
2018-07-28 23:12:30 +0800

    添加中英文语言支持文件

---
+++ goforeat_app/app/language/en.js
@@ -0,0 +1,152 @@
+
+export default {
+  goforeat: 'Goforeat',
+  myorder: 'My Order',
+  contact: 'Contact Us',
+  about: 'About Us',
+  setting: 'Setting',
+  payment: 'Payment Method',
+  help: 'Need Help?',
+  address: 'Choice address for orders',
+  all: 'Total',
+  delivering: 'Back Orders',
+  cancelOrder: 'Cancelled',
+  cash: 'Cash Pay',
+  credit: 'Pay with Credit Card',
+  manageCard: 'More Credit Cards Payment Options',
+  setCard: 'Set your credit card',
+  manageCardTitle: 'Credit Cards Payment Options',
+  cardUser: 'Name on card',
+  cardType: 'Card Type',
+  date: 'Expiration date',
+  card: 'Credit Card',
+  changeCard: 'Change Your Card',
+  addCard: 'Add a Credit or Debit Card',
+  bindCardOnce: 'Only one credit card can be bound',
+  addCardNow: 'Bind immediately',
+  cancelBind: 'Unbind',
+  nameRequire: 'Cardholder\'s Name-Please Fill in Your Name (Required)',
+  cardRequire: 'Card Number -Please Fill in Your Card Number',
+  timeRequire: 'Expiration date- Please Choose an Expiration date',
+  cvcRequire: 'Three-Digit CVC (Required)',
+  phone: 'Contact By Phone',
+  online: 'Chat Online',
+  notice: 'Notification',
+  lang: 'Language',
+  langChoose: 'Choose Language',
+  policy: 'Privacy Policy',
+  services: 'Terms & Conditions',
+  signInPhone: 'Sign in',
+  fillInPhone: 'Please Fill in Your Phone Number',
+  fillInCode: 'Please Fill in Your Verification Code',
+  sendCode: 'send',
+  loginOrRegister: 'Sign in/Enroll',
+  book: 'Book now',
+  detailPage: 'Check Out',
+  quantity: 'Quantity',
+  total: 'Grand Total',
+  foodInformation: 'Food Pick-up Information',
+  remark: 'Remark',
+  fooddate: 'Pick-up Date',
+  foodAddress: 'Pick-up Area',
+  foodTime: 'Pick-up Time',
+  bookPhone: 'My Phone Number',
+  orderDetail: 'Order Details',
+  foodDetail: 'Details',
+  endTime: 'Deadline',
+  mon: 'Monday',
+  tuse: 'Tuesday',
+  wed: 'Wednesday',
+  thr: 'Thursday',
+  fri: 'Friday',
+  sat: 'Saturday',
+  sun: 'Sunday',
+  menu: '\'s menu',
+  foodName: 'Items Ordered',
+  couponCode: 'Promo Code',
+  foodTotal: 'Total for this Order',
+  failToOrder: 'Failed to Check Out the Order',
+  sendOrder: 'Confirm Order',
+  orderNow: 'Order',
+  myOrder: 'Your Order',
+  send: 'Send',
+  logout: 'Logout',
+  use: 'use',
+  tips: 'tips',
+  cancel: 'cancel',
+  confirm: 'confirm',
+  userCancel: '用戶取消',
+  unconfirm: 'unconfirmed',
+  finish: 'finished',
+  paymentStatus: 'Payment status',
+  weekMenu: 'This week menu',
+  dailyFood: 'Daily takeaway',
+  login_tips: {
+    success: {
+      code: 'Verifying code has been sent successfully',
+      login: 'Logon successful'
+    },
+    fail: {
+      phone_format: 'Your phone number is not in the correct format',
+      phone_null: 'Please fill in your phone number',
+      code: 'Verifying code has not been sent successfully',
+      code_null: 'Please fill in your verifying code ',
+      login: 'Logon failed',
+    }
+  },
+  setting_tips: {
+    success: {
+      logout: 'Logout successful'
+    },
+    fail: {
+      logout: 'Logout failed',
+      logout_network: 'Logon failed. Please Login Again.'
+    },
+    common: {
+      logout_msg: 'Are you sure you want to log out?'
+    }
+  },
+  linking_tips: {
+    fail: {
+      not_support: 'There\'s no support for dialing',
+      call: 'Failed to dial the user number'
+    }
+  },
+  article_tips: {
+    fail: {
+      load: 'Failed to load the article'
+    }
+  },
+  confirmorder_tips: {
+    success: {
+      order: 'Order successfully',
+      coupon: 'Get a discount successfully'
+    },
+    fail: {
+      get_order: 'Failed to get the order information',
+      confirm_order: 'Failed to check out the order information',
+      order: 'Failed to place your order',
+      coupon_null: 'Please fill in your coupon code',
+      coupon_used: 'You have got this discount ',
+      get_coupon: 'Failed to discount'
+    }
+  },
+  myorder_tips: {
+    success: {
+      cancel_order: 'Cancel the order successfully'
+    },
+    common: {
+      cancel_order: 'Are you sure you want to cancel the order?',
+      cancel_order_btn: 'cancel order',
+    }
+  },
+  common_tips: {
+    err: 'An unknown error occurred',
+    network_err: 'Network request failed',
+    no_network: 'Network error...',
+    no_auth: 'Please log in first',
+    no_function: 'This is under construction',
+    reload: 'Loading failed, please click retry',
+    no_data: 'No data'
+  }
+}
---
+++ goforeat_app/app/language/zh.js
@@ -0,0 +1,151 @@
+export default {
+  goforeat: '有得食',
+  myorder: '我的訂單',
+  contact: '用戶支援',
+  about: '關於我們',
+  setting: '系統設置',
+  payment: '支付方式',
+  help: '用戶支援',
+  address: '請選擇取餐地點',
+  all: '全部',
+  delivering: '待配送',
+  cancelOrder: '已取消',
+  cash: '現金支付',
+  credit: '信用卡支付',
+  manageCard: '管理您的信用卡',
+  setCard: '設定您的信用卡',
+  manageCardTitle: '管理我的銀行卡',
+  cardUser: '持卡人姓名',
+  cardType: '卡片類型',
+  date: '有效期',
+  card: '信用卡',
+  changeCard: '更換信用卡',
+  bindCardOnce: '只可以綁定一張信用卡',
+  addCard: '綁定卡號',
+  addCardNow: '立即綁定', 
+  cancelBind: '取消綁定',
+  nameRequire: '請輸入(必填)',
+  cardRequire: '請輸入信用卡號',
+  timeRequire: '請選擇有效期',
+  cvcRequire: '請輸入(必填)',
+  phone: '電話聯繫',
+  online: '在線支援',
+  notice: '通知',
+  lang: '語言',
+  langChoose: '選擇語言',
+  policy: '隱私政策',
+  services: '服務條款',
+  signInPhone: '手機號登入',
+  fillInPhone: '請輸入手機號',
+  fillInCode: '請輸入驗證碼',
+  sendCode: '點擊發送',
+  loginOrRegister: '登錄/註冊',
+  book: '立即預訂',
+  detailPage: '訂單確認頁',
+  quantity: '數量',
+  total: '總金額',
+  foodInformation: '取餐資料',
+  remark: '送餐備註',
+  fooddate: '取餐日期',
+  foodAddress: '取餐地點',
+  foodTime: '預計取餐時間',
+  bookPhone: '下單電話',
+  orderDetail: '訂單詳情',
+  foodDetail: '詳情',
+  endTime: '截止時間',
+  mon: '星期一',
+  tuse: '星期二',
+  wed: '星期三',
+  thr: '星期四',
+  fri: '星期五',
+  sat: '星期六',
+  sun: '星期日',
+  menu: '的餐單',
+  foodName: '菜品名稱',
+  couponCode: '優惠碼',
+  foodTotal: '總計',
+  failToOrder: '確認訂單失敗',
+  sendOrder: '確認訂單',
+  orderNow: '立即下單',
+  myOrder: '您的訂單',
+  send: '發送',
+  logout: '登出',
+  use: '使用',
+  tips: '提示',
+  cancel: '取消',
+  confirm: '確定',
+  userCancel: '用戶取消',
+  unconfirm: '未確認',
+  finish: '已完成',
+  paymentStatus: '支付狀態',
+  weekMenu: '本週菜單',
+  dailyFood: '每日推薦',
+  login_tips: {
+    success: {
+      code: '驗證碼發送成功',
+      login: '登錄成功'
+    },
+    fail: {
+      phone_format: '手機號格式有誤',
+      phone_null: '請填寫手機號',
+      code: '驗證碼發送失敗',
+      code_null: '請填寫驗證碼',
+      login: '登錄失敗,請重新登錄',
+    }
+  },
+  setting_tips: {
+    success: {
+      logout: '登出成功'
+    },
+    fail: {
+      logout: '登出失敗',
+      logout_network: '登出失敗,請檢查網絡'
+    },
+    common: {
+      logout_msg: '確認要登出嗎?'
+    }
+  },
+  linking_tips: {
+    fail: {
+      not_support: '不支持撥打電話',
+      call: '撥打用戶號碼失敗'
+    }
+  },
+  article_tips: {
+    fail: {
+      load: '加載文章失敗'
+    }
+  },
+  confirmorder_tips: {
+    success: {
+      order: '下單成功',
+      coupon: '優惠成功'
+    },
+    fail: {
+      get_order: '獲取訂單信息失敗',
+      confirm_order: '確認訂單失敗',
+      order: '下單失敗',
+      coupon_null: '請輸入優惠碼',
+      coupon_used: '您已經優惠過了',
+      get_coupon: '獲取優惠失敗'
+    }
+  },
+  myorder_tips: {
+    success: {
+      cancel_order: '取消訂單成功'
+    },
+    common: {
+      cancel_order: '確定要取消訂單嗎？',
+      cancel_order_btn: '取消訂單',
+    }
+  },
+  common_tips: {
+    err: '發生未知錯誤',
+    network_err: '网络請求失敗',
+    no_network: '網絡飛走了...',
+    no_auth: '請先登錄哦',
+    no_function: '該功能暫未開放',
+    reload: '加載失敗,請點擊重試',
+    no_data: '暫無數據'
+  }
+}
