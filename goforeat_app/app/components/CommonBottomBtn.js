a90edc08778550b926a105f5bbfcd812b815eede
syun <517897442@qq.com>
2018-08-11 17:32:26 +0800

    修复渐变色组件warning

--- goforeat_app/app/components/CommonBottomBtn.js
+++ goforeat_app/app/components/CommonBottomBtn.js
@@ -20,7 +20,7 @@ const CommonBottomBtn = props => {
     <View style={CommonStyles.common_btn_container}>
       <TouchableOpacity onPress={props.clickFunc}>
         <LinearGradient colors={['#FF9F48','#FF4141']} start={{x:0.0, y:0.0}} end={{x:1.0,y: 0.0}} style={[CommonStyles.btn,props.style]}>
-          <Text style={{color:'#fff',fontSize:16}}>{props.children}</Text>
+          <Text style={{color:'#fff',fontSize:16,backgroundColor:'transparent'}}>{props.children}</Text>
         </LinearGradient>
       </TouchableOpacity>
     </View>
