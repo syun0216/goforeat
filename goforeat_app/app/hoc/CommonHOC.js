9f4df040e5850bce43f84df15464b624a446e98c
syun <517897442@qq.com>
2018-11-29 08:52:56 +0800

    高阶组件去除评论模块

--- goforeat_app/app/hoc/CommonHOC.js
+++ goforeat_app/app/hoc/CommonHOC.js
@@ -12,7 +12,7 @@ import JPushModule from 'jpush-react-native';
 import {em, _winWidth, _winHeight} from '../utils/global_params';
 import ToastUtils from '../utils/ToastUtil';
 //components
-import CommonBottomBtn from '../components/CommonBottomBtn';
+import CommonComment from '../components/CommonComment';
 import LoadingModal from '../components/LoadingModal';
 //language
 import I18n from '../language/i18n';
@@ -31,14 +31,7 @@ const shareOptions = { //分享優惠券信息
   title: '分享有得食搶優惠券'
 };
 
-const commentKeyWord = {
-  0: '暫不評論',
-  1: '非常差',
-  2: '差',
-  3: '一般',
-  4: '好',
-  5: '非常好'
-};
+
 
 const jpushCommonUrlDefined = {
   url: 1, // web页面
@@ -66,12 +59,7 @@ const CommonHOC = WarppedComponent => class extends Component {
 
   constructor(props) {
     super(props);
-    this.commentText = '';
-    this.isCommentSubmit = false;
     this.state = {
-      currentComment:null,
-      currentStar: 5,
-      btnContent: '推薦好友領優惠券',
       i18n: I18n[props.screenProps.language],
       LoadingModal: false
     };  
@@ -79,7 +67,6 @@ const CommonHOC = WarppedComponent => class extends Component {
 
   componentDidMount() {
     this._jpushDidMount();
-    this._commentPopup();
     AppState.addEventListener('change',(nextState) => {
       if(nextState == 'active') {
         // this._commentPopup();
@@ -105,54 +92,6 @@ const CommonHOC = WarppedComponent => class extends Component {
   }
 
   //api
-  _commentPopup() {
-    popupComment().then(data => {
-      if(data.ro.respCode == '0000') {
-        this.setState({
-          currentComment: data.data
-        },() => {
-          this.popupDialog.show();
-        })
-      }
-    });
-  }
-
-  _addCommentApi() {
-    if(this.state.currentComment == null) {
-      this.popupDialog.dismiss();
-      return;
-    };
-    let {currentComment: {orderId}, currentStar} = this.state;
-    addComment(orderId, currentStar, this.commentText).then(data => {
-      if(data.ro.ok) {
-        this.isCommentSubmit = true;
-        this.popupDialog.dismiss();
-      } else {
-        ToastUtils.showWithMessage(data.ro.respMsg);
-      }
-    });
-  }
-
-  _addComment() {
-    this._addCommentApi();
-    if(this.state.currentStar == 5) {
-      Share.open(shareOptions).catch((err) => { 
-        console.log({err});
-      });
-    }
-  }
-  
-  _getComment(val) {
-    this.commentText = val;
-  }
-
-  _handleDialogDismiss() {
-    this.setState({
-      currentStar: 0
-    }, () => {
-      this._addCommentApi();
-    });
-  }
 
   _showLoadingModal() {
     this.setState({
@@ -283,143 +222,18 @@ const CommonHOC = WarppedComponent => class extends Component {
   }
   // ---------------------------end
 
-  _renderEmojiBtn({defaultImage, activeImage, name, val}) {
-    return (
-      <TouchableWithoutFeedback key={name} onPress={() => {
-        this.setState({
-          currentStar: val
-        },() => {
-          if(this.state.currentStar == 5) {
-            this.setState({
-              btnContent: '推薦好友領優惠券'
-            })
-          } else {
-            this.setState({
-              btnContent: '確認提交'
-            })
-          }
-        })
-      }} style={styles.emoji}>
-        <Image source={this.state.currentStar == val ? activeImage : defaultImage} style={styles.emoji}/>
-      </TouchableWithoutFeedback>
-    )
-  }
-
-  _renderPopupDialogView() {
-    const emoji_arr = [
-      {defaultImage: require('../asset/crazy-normal.png'), activeImage: require('../asset/crazy-press.png'), name: 'crazy', val: 1},
-      {defaultImage: require('../asset/hard-normal.png'), activeImage: require('../asset/hard-press.png'), name: 'hard', val: 2},
-      {defaultImage: require('../asset/allright-normal.png'), activeImage: require('../asset/allright-press.png'), name: 'allright', val: 3},
-      {defaultImage: require('../asset/well-normal.png'), activeImage: require('../asset/well-press.png'), name: 'well', val: 4},
-      {defaultImage: require('../asset/delicious-normal.png'), activeImage: require('../asset/delicious-press.png'), name: 'delicious', val: 5},
-    ];
-    const {currentStar,currentComment: {orderName, picture}} = this.state;
-    const defaultImg = "https://img.xiumi.us/xmi/ua/18Wf8/i/98c314a76260a9634beecfd27c28770d-sz_80962.jpg?x-oss-process=style/xmr";
-
-    return (
-      <PopupDialog
-        ref={(popupDialog) => { this.popupDialog = popupDialog; }}
-        width={em(295)}
-        height={em(435)}
-        onDismissed={() => this._handleDialogDismiss()}
-        dialogStyle={styles.popupDialogContainer}
-        dialogAnimation={slideAnimation}
-      >
-        <Image style={styles.topImg} reasizeMode="contain" source={require('../asset/commentTop.png')}/>
-        <View style={styles.topTitle}>
-          <Text style={styles.topTitleText}>给</Text>
-          <Text style={[styles.topTitleText, styles.foodNameText]} numberOfLines={1}>{orderName || '有得食'}</Text>
-          <Text style={styles.topTitleText}>打分</Text>
-        </View>
-        <View style={styles.content}>
-          <Image style={styles.contentImg} source={{uri: picture || defaultImg}}/>
-          <View style={styles.emojiContainer}>
-            {
-              emoji_arr.map(v => this._renderEmojiBtn(v))
-            }
-            </View>
-          {/*<Text style={[styles.commentText,{color: currentStar < 3 ? '#ccc' : currentStar<= 4 ? '#f7ba2a' : '#ff630f'}]}>{commentKeyWord[currentStar]}</Text>*/}
-          <TextInput allowFontScaling={false} style={styles.Input} underlineColorAndroid="transparent" placeholderTextColor="#9d9d9d" 
-          placeholder={`例如:${commentKeyWord[currentStar]}`} clearButtonMode="while-editing" onChangeText={(val) => this._getComment(val)}/>
-          <CommonBottomBtn clickFunc={() => this._addComment()} style={{width: em(263)}}>{this.state.btnContent}</CommonBottomBtn>
-        </View>
-      </PopupDialog>
-    )
-  }
-
   render() {
     let {i18n} = this.state;
-    
     return (
       <Container>
-        {this.state.currentComment != null ? this._renderPopupDialogView() : null}
         {this.state.LoadingModal ? <LoadingModal /> : null}
+        <CommonComment />
         <WarppedComponent ref={w => this.WarppedComponent = w} {...this.props} showLoading={this._showLoadingModal.bind(this)} hideLoading={this._hideLoadingModal.bind(this)} i18n={i18n}/>
       </Container>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  popupDialogContainer: {
-    position: 'relative',
-    // borderWidth: 10,
-    // borderColor: 'transparent',
-  },
-  topImg: {
-    width: '100%',
-    height: em(99),
-    position: 'absolute',
-    top: em(-22),
-    left: 0
-  },
-  topTitle: {
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    height: em(75),
-    flexDirection: 'row'
-  },
-  topTitleText: {
-    color: '#ef3f15',
-    fontSize: em(17),
-    marginBottom: em(10),
-  },
-  foodNameText: {
-    marginRight: em(8),
-    marginLeft: em(8),
-    maxWidth: em(100)
-  },
-  content: {
-    padding: em(16)
-  },
-  contentImg: {
-    width: '100%',
-    height: em(146),
-    borderRadius: em(15),
-  },
-  emojiContainer:{
-    flexDirection: 'row',
-    marginTop: em(12),
-    marginBottom: em(24),
-    justifyContent: 'space-between'
-  },
-  emoji: {
-    width: em(35),
-    height: em(35)
-  },
-  commentText: {
-    textAlign: 'center',
-    marginBottom: em(10),
-  },
-  Input: {
-    color: '#9d9d9d',
-    fontSize: em(13),
-    height: Platform.OS == 'ios' ? em(30) : 40 * (_winHeight / 592),
-    width: _winWidth * 0.85,
-    borderBottomWidth: 1,
-    borderBottomColor: '#9d9d9d',
-    marginBottom: Platform.OS == "android"?0:em(10),
-  }
-})
+
 
 export default CommonHOC;
