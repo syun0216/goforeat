45d467c1a830329a86092d358a63b3121bc26996
syun <517897442@qq.com>
2018-12-21 11:04:59 +0800

    删除不必要props

--- app/DashBoardView.js
+++ app/DashBoardView.js
@@ -8,12 +8,9 @@ import {
   STOCK_PLACE,
   DELETE_PLACE,
   REFRESH,
-  CHANGE_THEME,
   SET_PAY_TYPE,
   SET_CREDIT_CARD,
-  REMOVE_CREDIT_CARD,
-  SHOW_AD,
-  HIDE_AD
+  REMOVE_CREDIT_CARD
 } from "./actions";
 
 class DashBoardView extends PureComponent {
@@ -39,12 +36,8 @@ const dashboardStateToProps = (state) => {
   toPage: state.login.toPage,
   userInfo: state.auth,
   sid: state.auth.sid,
-  refresh: state.refresh.refreshParams,
   language: state.language.language,
-  place: state.placeSetting.place,
   paytype: state.payType.payType,
-  creditCardInfo: state.creditCardInfo.creditCardInfo,
-  isAdShow: state.toggleAd.isAdvertisementShow
 })}
 
 const dashboardmapDispatchToProps = dispatch => ({
@@ -52,15 +45,8 @@ const dashboardmapDispatchToProps = dispatch => ({
   userLogout: () => dispatch({type:LOGOUT}),
   toggleLogin: (status) => dispatch({type:'CHANGE_LOGIN_STATUS', showLogin: status}),
   setToPageParams: (toPage) => dispatch({type:'SET_LOGIN_TO_PAGE', toPage}),
-  refreshReset: (val) => dispatch({type:REFRESH,refresh:val}),
   changeLanguage: (language) => dispatch({type: CHANGE_LANGUAGE,language}),
-  stockPlace: (place) => dispatch({type: STOCK_PLACE,place}),
-  deletePlace: () => dispatch({type: DELETE_PLACE}),
   setPayType: (paytype,callback) => dispatch({type: SET_PAY_TYPE,paytype,callback}),
-  setCreditCardInfo: (creditCardInfo) => dispatch({type: SET_CREDIT_CARD, creditCardInfo}),
-  removeCreditCardInfo: () => dispatch({type: REMOVE_CREDIT_CARD}),
-  showAd: () => dispatch({type: SHOW_AD}),
-  hideAd: () => dispatch({type: HIDE_AD}),
   dispatch: dispatch
 })
 
