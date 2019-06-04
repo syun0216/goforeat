0677629d9c344bc4aaa8e55c68d6bdf17b646a0e
syun <517897442@qq.com>
2019-06-04 09:16:32 +0800

    deviceInfo 监测是否为debug版本

--- app/utils/DeviceInfo.js
+++ app/utils/DeviceInfo.js
@@ -11,3 +11,8 @@ export function getLanguage() {
 export function getDeviceId() {
   return DeviceInfo.getDeviceId();
 }
+
+export function isDebugVersion() {
+  let _version = DeviceInfo.getVersion();
+  return _version.split('.')[0] == 0; //以0开头的则为debug版本 其他为正式版
+}
