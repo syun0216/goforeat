83a7d01671dccd1a3431667a1ef22eeedc1beb5c
syun <517897442@qq.com>
2019-06-04 09:19:31 +0800

    ios 和android的基本配置更新

--- android/app/src/main/java/com/goforeat_app/MainApplication.java
+++ android/app/src/main/java/com/goforeat_app/MainApplication.java
@@ -3,6 +3,7 @@ package com.goforeat_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.theweflex.react.WeChatPackage;
@@ -75,6 +76,7 @@ public class MainApplication extends Application implements ReactApplication {
             String deploymentKey = "fMXsr1oL8ExCRlmMZD2nLEWHY0-rd261912e-873f-4270-b887-25c360664c8c";
             return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new RNCWebViewPackage(),
             new RNFusedLocationPackage(),
             new VectorIconsPackage(),
             new FastImageViewPackage(),
