ec5bdd2c5956424ce946b6806d39c617606c5ff1
syun <517897442@qq.com>
2019-06-26 11:36:36 +0800

    add api

---
+++ app/api/config.js
@@ -0,0 +1,103 @@
+import axios from 'axios';
+import { Platform, AsyncStorage } from 'react-native';
+import store from '../store/index';
+import qs from 'qs';
+//actions
+import { HIDE_LOADING_MODAL, SHOW_LOADING_MODAL, LOGOUT, SHOW_LOADING, HIDE_LOADING } from '../actions'
+//language
+import i18n from '../language/i18n';
+//utils 
+import ToastUtil from '../utils/ToastUtil';
+import { getVersion } from '../utils/DeviceInfo';
+import { currentPlatform } from '../utils/global_params';
+import NavigationService from '../utils/NavigationService';
+
+const CancelToken = axios.CancelToken;
+export const source = CancelToken.source();
+
+export const netWorkFailCode = 50000; //custom network error code
+
+const BASE_URL = 'https://api.goforeat.hk';
+
+// const generateBaseUrl = async () => {
+//     let res = await AsyncStorage.getItem('storage_debug');
+//     console.log('res :', res);
+// }
+
+// generateBaseUrl();
+
+let service = axios.create({
+    baseURL: BASE_URL,
+    timeout: 4500
+});
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+service.interceptors.request.use(config => {
+    if(!store.getState().loading.showLoadingModal && config.loading) {
+        store.dispatch({type: SHOW_LOADING});
+    }
+    if (config.method == 'post') {
+        let _data = qs.parse(config.data);
+        config.data = qs.stringify({
+            ..._data,
+            sid: store.getState().auth.sid,
+            language: store.getState().language.language,
+            sellClient: currentPlatform,
+            appVersion: getVersion()
+        })
+    } else if (config.method == 'get') {
+        config.params = {
+            ...config.params,
+            sid: store.getState().auth.sid,
+            language: store.getState().language.language,
+            sellClient: currentPlatform,
+            appVersion: getVersion()
+        }
+    }
+    console.log(`'%c request'${config.url}`,'background:#000;color:#bada55',config);
+    // config.cancelToken = new CancelToken(function executor(c) {
+
+    // })
+    return config;
+}, error => {
+    // Do something with request error
+    ToastUtil.showWithMessage(i18n[store.getState().language.language].common_tips.network_err);
+    Promise.reject(error)
+})
+
+let _responseTimer = null;
+
+service.interceptors.response.use(response => {
+    console.log(`%c reponse'${response.config.url}`,'background:#000;color:#bada55',response);
+    if (response.status == 200) {
+        if(_responseTimer) clearTimeout(_responseTimer);
+        _responseTimer = setTimeout(() => {
+            store.dispatch({type: HIDE_LOADING});
+            store.dispatch({type: HIDE_LOADING_MODAL});
+            clearTimeout(_responseTimer);
+        },500);
+        const res = response.data;
+        if(res.ro.respCode === "0000") {
+            return res.data;
+        }else if(res.ro.respCode === "10006" || res.ro.respCode === "10007") {
+            store.dispatch({type:LOGOUT});
+            NavigationService.back();
+            // console.log(NavigationService.navigatorRef)
+        }
+        ToastUtil.showWithMessage(res.ro.respMsg);
+        return Promise.reject({errCode: res.ro.respCode, errMsg: res.ro.respMsg});
+    }
+}, err => {
+    // console.log({err})
+    store.dispatch({type: HIDE_LOADING})
+    ToastUtil.showWithMessage(i18n[store.getState().language.language].common_tips.network_err);
+    return Promise.reject({errCode: netWorkFailCode, errMsg: i18n[store.getState().language.language].common_tips.network_err});
+})
+
+export const reinitServer = url => {
+    service.defaults.baseURL = url
+    // console.log('service :', service);
+}
+
+
+export default service;
---
+++ app/api/request.js
@@ -0,0 +1,599 @@
+import request from './config';
+import axios from 'axios';
+import store from '../store/index';
+import {getVersion} from '../utils/DeviceInfo';
+import {currentPlatform} from '../utils/global_params';
+import qs from 'qs';
+//login
+export function getCode(mobile, type) {
+  return request({
+    url: '/passport/register',
+    loading: true,
+    method: 'post',
+    data: {
+      mobile,
+      type
+    }
+  })
+}
+
+export function register(mobile, type, token, code, password) {
+  return request({
+    url: '/passport/checkCode',
+    loading: true,
+    method: 'post',
+    data: {
+      mobile,
+      type,
+      token,
+      code,
+      password
+    }
+  })
+}
+
+export function login(mobile, type, password) {
+  return request({
+    url: '/passport/login',
+    loading: true,
+    method: 'post',
+    data: {
+      mobile,
+      type,
+      password
+    }
+  })
+}
+
+export function checkCode(mobile, type, token, code) {
+  return request({
+    url: '/passport/checkCode',
+    loading: true,
+    method: 'post',
+    data: {
+      mobile,
+      type,
+      token,
+      code
+    }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/passport/logout',
+    loading: true,
+    method: 'post'
+  })
+}
+
+//article
+export function getArticleList(offset, placeId) {
+  return request({
+    url: '/cms/getNewsList',
+    loading: true,
+    method: 'post',
+    data: {
+      limit: 5,
+      offset,
+      placeId
+    }
+  })
+}
+
+export function getNewArticleList({offset, placeId}) {
+  return request({
+    url: '/food/getFoodList',
+    loading: false,
+    method: 'get',
+    params: {
+      offset,
+      limit: 5,
+      placeId
+    }
+  })
+}
+
+//homepage
+export function getDailyFoods(placeId) { //被摒弃
+  return request({
+    url: '/food/getDailyFood',
+    loading: false,
+    method: 'get',
+    params: {
+      placeId
+    }
+  })
+}
+
+export function getFoodDetails(dateFoodId) {
+  return request({
+    url: '/food/getFoodDetail',
+    loading: false,
+    method: 'post',
+    data: {
+      dateFoodId
+    }
+  })
+}
+
+//confirm order
+export function createOrder(foodId, placeId, amount) { //被摒弃
+  return request({
+    url: '/order/create',
+    loading: true,
+    method: 'get',
+    params: {
+      foodId,
+      placeId,
+      amount
+    }
+  })
+}
+
+export function createNewOrder(dateFoodId, amount, placeId, addStatus) {
+  return request({
+    url: '/order/createNew',
+    loading: false,
+    method: 'post',
+    data: {
+      dateFoodId,
+      amount,
+      placeId,
+      addStatus
+    }
+  })
+}
+/**
+ * 确认下单（旧）
+ *
+ * @export
+ * @param {*} orderId
+ * @param {*} payMoney
+ * @param {number} [payment=1]
+ * @param {*} token
+ * @param {*} remark
+ * @param {*} [deductionId=null]
+ * @param {*} [placeId=null]
+ * @returns
+ */
+export function confirmOrder(orderId, payMoney, payment = 1, token, remark, deductionId=null,placeId=null) {
+  return request({
+    url: '/order/confirm',
+    loading: true,
+    method: 'post',
+    data: {
+      orderId,
+      payMoney,
+      payment,
+      token,
+      remark,
+      deductionId,
+      placeId
+    }
+  })
+}
+/**
+ * 确认下单接口（新）
+ *
+ * @export
+ * @param {*} orderId
+ * @param {*} payMoney
+ * @param {number} [payment=1]
+ * @param {*} token
+ * @param {*} [deductionId=null]
+ * @param {*} useMonthTicket
+ * @param {*} remark
+ * @returns
+ */
+export function confirmOrderV1(orderId, payMoney, payment=1, token, deductionId = null, useMonthTicket, remark) {
+  return request({
+    url: '/order/v1/confirm',
+    loading: true,
+    method: 'post',
+    data: {
+      orderId,
+      payMoney,
+      payment,
+      token,
+      deductionId,
+      useMonthTicket,
+      remark
+    }
+  })
+}
+
+export function useCoupon(coupon,orderId) {
+  return request({
+    url: '/coupon/isUseful',
+    loading: true,
+    method: 'post',
+    data: {
+      coupon,
+      orderId
+    }
+  })
+}
+
+//myorder
+export function cancelOrder(orderId) {
+  return request({
+    url: '/order/cancel',
+    loading: true,
+    method: 'post',
+    data: {
+      orderId
+    }
+  })
+}
+
+export function myOrder({offset, status}) {
+  return request({
+    url: '/order/myOrderList',
+    loading: false,
+    method: 'post',
+    data: {
+      limit: 5,
+      offset,
+      status
+    }
+  })
+}
+
+//place picker
+export function foodPlaces(lat = null, lon = null) {
+  return request({
+    url: '/food/getDeliveryPlace',
+    loading: true,
+    method: 'get',
+    params: {
+      lat,
+      lon
+    }
+  })
+}
+
+export function saveDevices(registrationId) {
+  return request({
+    url: '/device/save',
+    loading: true,
+    method: 'post',
+    data: {
+      registrationId
+    }
+  })
+}
+
+//credit card
+export function vaildCard(bankCard) {
+  return request({
+    url: '/member/isBankCardValid',
+    loading: true,
+    method: 'post',
+    data: {
+      bankCard
+    }
+  })
+}
+
+//feed back
+export function feedback(data) {
+  return request({
+    url: '/feedback/add',
+    loading: true,
+    method: 'post',
+    data: data
+  })
+}
+
+//广告位
+export function adSpace() {
+  return request({
+    url: '/adSpace/list',
+    loading: true,
+    method: 'post',
+    data: {
+      pagePosition: 1
+    }
+  })
+}
+
+//首页公告栏
+export function queryLatest() {
+  return request({
+    url: '/notice/queryLatest',
+    loading: true,
+    method: 'get'
+  })
+}
+
+export function queryList() {
+  return request({
+    url: '/notice/queryList',
+    loading: true,
+    method: 'get'
+  })
+}
+
+//pay setting
+export function getPaySetting() {
+  return request({
+    url: '/member/myPayments',
+    loading: true,
+    method: 'get'
+  })
+}
+/**
+ * 新的支付方式
+ *
+ * @export
+ * @returns
+ */
+export function getPaySettingNew() {
+  return request({
+    url: '/member/myPaymentNew',
+    loading: true,
+    method: 'post'
+  })
+}
+
+//修改支付方式
+export function setPayment(payment) {
+  return request({
+    url: '/member/setPayment',
+    loading: true,
+    method: 'post',
+    data: { payment }
+  })
+}
+
+//月票
+export function getMonthTicket() {
+  return request({
+    url: '/member/myMonthTicket',
+    loading: true,
+    method: 'get'
+  })
+}
+
+//我的资料
+export function getMyInfo() {
+  return request({
+    url: '/member/me',
+    loading: true,
+    method: 'get'
+  })
+}
+
+export function updateMyInfo({nickName, address, email, gender}) {
+  return request({
+    url: '/member/update',
+    loading: true,
+    method: 'post',
+    data: {
+      nickName,
+      address,
+      email,
+      gender
+    }
+  })
+}
+
+export function uploadAvatar(profileImg) {
+  let _img = {
+      name: profileImg.fileName,
+      uri: profileImg.uri
+    };
+  let params = new FormData();
+  params.append('profileImg', _img);
+  params.append('sid', store.getState().auth.sid);
+  params.append('language', store.getState().language.language);
+  params.append('sellClient', currentPlatform);
+  params.append('appVersion', getVersion());
+  console.log('params',JSON.stringify(params))
+  return axios.post('http://api.goforeat.hk/member/uploadProfileImg', params)
+  // return request({
+  //   url: '/member/uploadProfileImg',
+  //   method: 'post',
+  //   headers: { 'content-type': 'multipart/form-data' },
+  //   data: {
+  //     'profileImg': _img
+  //   }
+  // })
+}
+
+export function myCoupon({offset,limit=5,useStatus =null,payMoney=null}) {
+  return request({
+    url: '/coupon/myCoupon',
+    loading: false,
+    method: 'post',
+    data: {
+      limit,
+      offset,
+      useStatus,
+      payMoney
+    }
+  })
+}
+
+export function getCoupon(exchangeCode) {
+  return request({
+    url: '/coupon/exchangeCoupon',
+    loading: true,
+    method: 'post',
+    data: {
+      exchangeCode
+    }
+  })
+}
+
+//点赞
+export function myFavorite(foodId, status) {
+  return request({
+    url: '/food/like',
+    loading: false,
+    method: 'post',
+    data: {
+      foodId,
+      status
+    }
+  })
+}
+
+//弹窗点评
+export function popupComment() {
+  return request({
+    url: '/comment/isPopup',
+    loading: true,
+    method: 'post'
+  })
+}
+
+export function addComment(orderId, star, comment) {
+  return request({
+    url: '/comment/add',
+    loading: true,
+    method: 'post',
+    data: {
+      orderId,
+      star,
+      comment
+    }
+  })
+}
+
+//保存信用卡
+export function setCreditCard(token, time, tailNum) {
+  return request({
+    url: '/member/setCreditCard',
+    loading: true,
+    method: 'post',
+    data: {
+      token, time, tailNum
+    }
+  })
+}
+
+//获取信用卡
+export function getCreditCard() {
+  return request({
+    url: '/member/myCreditCard',
+    loading: true,
+    method: 'get',
+  })
+}
+
+//取消绑定信用卡
+export function removeCreditCard() {
+  return request({
+    url: '/member/cancelCreditCard',
+    loading: true,
+    method: 'post'
+  })
+}
+
+/**
+ * 获取月票列表
+ *
+ * @export
+ * @returns
+ */
+export function getMonthTicketList() {
+  return request({
+    url: '/monthTicket/getSpecList',
+    loading: true,
+    method: 'get'
+  })
+}
+
+/**
+ * 获取月票须知
+ *
+ * @export
+ * @returns
+ */
+export function getMonthTicketInfo() {
+  return request({
+    url: '/monthTicket/info',
+    loading: true,
+    method: 'post'
+  })
+}
+
+export function createMonthTicket(specId) {
+  return request({
+    url: '/monthTicket/create',
+    loading: true,
+    method: 'post',
+    data: {
+      specId
+    }
+  })
+}
+
+export function confirmMonthTicket({orderId, payMoney, payment, token}) {
+  return request({
+    url: '/monthTicket/confirm',
+    loading: true,
+    method: 'post',
+    data: {
+      orderId, payMoney, payment, token
+    }
+  })
+}
+/**
+ * 通过订单id 获取评论详情
+ *
+ * @export
+ * @param {*} orderId
+ * @returns
+ */
+export function getCommentByOrderId(orderId) {
+  return request({
+    url: '/comment/getComment',
+    loading: true,
+    method: 'post',
+    data: {
+      orderId
+    }
+  })
+}
+/**
+ * 查看菜品评论
+ *
+ * @export
+ * @param {*} {foodId, limit, offset}
+ * @returns
+ */
+export function getCommentList({foodId, limit, offset}) {
+  return request({
+    url: '/comment/getCommentList',
+    loading: false,
+    method: 'post',
+    data: {
+      foodId,
+      limit,
+      offset
+    }
+  })
+}
+
+/**
+ * 获取版本更新信息
+ *
+ * @export
+ * @returns
+ */
+export function getVersionFromServer() {
+  return request({
+    url: '/info/getVersionInfo',
+    loading: true,
+    method: 'post'
+  })
+}
+
+export const versionCode = {
+  normal: 1,
+  alertToUpdate: 2,
+  mustUpdate: 3
+};
