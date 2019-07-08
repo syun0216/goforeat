a999fa22a6ec369e441ad443d49429c02b3936f8
syun <517897442@qq.com>
2019-07-08 14:05:39 +0800

    内容页兼容活动信息查看

--- app/views/ContentView.js
+++ app/views/ContentView.js
@@ -2,13 +2,11 @@ import React, { Component } from "react";
 import {
   View,
   WebView,
-  AlertIOS,
-  ToastAndroid,
+  TouchableWithoutFeedback,
   Clipboard,
-  Platform,
   ActivityIndicator
 } from "react-native";
-import { Container, Text } from "native-base";
+import { Text } from "native-base";
 import Share, { ShareSheet, Button as SButton } from "react-native-share";
 //utils
 import ToastUtil from "../utils/ToastUtil";
@@ -19,21 +17,38 @@ import source from "../api/CancelToken";
 import Loading from "../components/Loading";
 import CommonHeader from "../components/CommonHeader";
 import CustomizeContainer from "../components/CustomizeContainer";
+import ShareComponent from "../components/ShareComponent";
+import ErrorPage from "../components/ErrorPage";
 //language
 import I18n from "../language/i18n";
+//api
+import {myInvites} from "../api/request";
+
+const WeChat = require("react-native-wechat");
 
 export default class ContentView extends Component {
+  _webView = null;
+  _shareList = null;
+  _inviteUrl = null;
   state = {
     shareboxVisible: false,
     modalVisible: false,
     loading: false,
     isError: false,
-    i18n: I18n[this.props.screenProps.language]
+    i18n: I18n[this.props.screenProps.language],
+    isShareListShow: false
   };
 
-  // componentDidMount() {
-  //   this.props.navigation.setParams({visible: true})
-  // }
+  constructor(props) {
+    super(props);
+    WeChat.registerApp("wx5b3f09ef08ffa7a7"); 
+  }
+
+  componentDidMount() {
+    if(this.props.navigation.state.params.kind === "activity") {
+      this._myInvites();
+    }
+  }
 
   componentWillUnmount() {
     source.cancel();
@@ -41,15 +56,33 @@ export default class ContentView extends Component {
 
   //api
 
+  _myInvites() {
+    myInvites().then(data => {
+      this._inviteUrl = data.inviteUrl;
+    })
+  }
+
   openShare = () => {
     if (this.props.navigation.state.params.kind === "canteen") {
       ToastUtil.showWithMessage("暫未開放分享店鋪");
       return;
+    }else if(this.props.navigation.state.params.kind == "activity") {
+      this.setState({
+        isShareListShow: true
+      });
+      this._shareList && this._shareList._toShowShareListView();
+      return;
+    }else {
+      this.setState({ shareboxVisible: true });
     }
-    this.setState({ shareboxVisible: true });
   };
 
   cancelShare = () => this.setState({ shareboxVisible: false });
+
+  handleMessage(event) {
+    console.log('event', event);
+    this.openShare();
+  }
 
   _shareLink = type => {
     let { data } = this.props.navigation.state.params;
@@ -81,6 +114,68 @@ export default class ContentView extends Component {
       }, 300);
     }
   };
+
+  _pressToShare = type => {
+    if(this._inviteUrl) {
+      switch(type) {
+        case 'whatsapp': {
+          const shareOptions = {
+            //分享優惠券信息
+            url: this._inviteUrl,
+            message: "點擊下面的連接，即可註冊探索更多美味：附上連接",
+            title: "Hi! 發現係有得食APP上註冊可以活動HKD35優惠券組合，訂購午餐時可以抵扣相應優惠券",
+            social: "whatsapp"
+          };
+          this.timer = setTimeout(() => {
+            Share.shareSingle(
+              Object.assign(shareOptions, {
+                social: "whatsapp"
+              })
+            )
+              .then(info => {
+                // console.log(info);
+                this.setState({
+                  modalVisible: false
+                });
+              })
+              .catch(err => {
+                alert(`WhatsApp:${err && err.error && err.error.message}`);
+                // console.log(err);
+                return;
+              });
+          }, 300);
+        };break;
+        case 'wechat': {
+          WeChat.isWXAppInstalled().then(isInstalled => {
+            if (isInstalled) {
+              let _obj = {
+                title: "註冊即領HKD35午餐優惠券組合",
+                description: '每日專注幫你好好食晏',
+                thumbImage: 'http://118.25.159.37:15108/images/icon_app.png',
+                type: "news",
+                webpageUrl: this._inviteUrl
+              };
+              // Platform.OS=='ios' && (
+              //   _obj['thumbImage'] = extralImage[0]
+              // );
+              WeChat.shareToSession(_obj).catch(error => {
+                // console.log(error);
+                if (error.message == -2) {
+                  this.props.toast("分享失敗");
+                } else {
+                  this.props.toast("分享成功");
+                }
+              });
+            } else {
+              this.props.toast("WeChat is not installed");
+            }
+          });
+        };break;
+      }
+    }else {
+      this.props.toast('初始化失敗,請退出重試');
+    }
+  }
 
   _renderShareSheet = () => {
     return (
@@ -146,12 +241,44 @@ export default class ContentView extends Component {
     );
   }
 
+  _renderPreventClickView() {
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => {
+          this.setState(
+            {
+              isShareListShow: false
+            },
+            () => {
+              this._shareList && this._shareList._toHideShareListView();
+            }
+          );
+        }}
+      >
+        <View
+          style={{
+            width: GLOBAL_PARAMS._winWidth,
+            height: GLOBAL_PARAMS._winHeight,
+            position: "absolute",
+            zIndex: 99,
+            top: 0,
+            left: 0,
+            backgroundColor: "#000",
+            opacity: 0.3
+          }}
+        />
+      </TouchableWithoutFeedback>
+    );
+  }
+
   _renderArticleContentView = () => (
     <WebView
+      ref={w => this._webView = w}
       bounces={true}
       scalesPageToFit={true}
       source={{ uri: this.props.navigation.state.params.data.url }}
       onError={() => this.setState({ isError: true })}
+      onMessage={(event) => this.handleMessage(event)}
       renderLoading={() => <Loading />}
       startInLoadingState={true}
       style={{
@@ -162,12 +289,12 @@ export default class ContentView extends Component {
   );
 
   render() {
-    let { i18n } = this.state;
     let {
       data: { title, food_title },
       kind
     } = this.props.navigation.state.params;
     title = typeof title == "undefined" ? "有得食" : title;
+    console.log(this.props.navigation.state.params);
     return (
       <CustomizeContainer.SafeView mode="linear">
         <CommonHeader
@@ -175,13 +302,29 @@ export default class ContentView extends Component {
           canBack
           hasRight
           rightIcon="sharealt"
-          rightClick={() => this.setState({ shareboxVisible: true })}
+          rightClick={() => {
+            this.openShare()
+          }}
         />
         {this.state.loading ? <Loading /> : null}
         <View style={{ flex: 1 }}>
+          {this.state.isShareListShow && this._renderPreventClickView()}
           {this._renderArticleContentView()}
           {this._renderShareSheet()}
+          <ShareComponent 
+            ref={sl => this._shareList = sl}
+            title="與好友分享優惠券"
+            getShareType={type => this._pressToShare(type)}
+          />
         </View>
+        {
+          this.state.isError && (<ErrorPage errorTips="加載失敗,請點擊重試..." errorToDo={() => {
+            this._webView && this._webView.reload();
+            this.setState({
+              isError: false
+            });
+          }}/>)
+        }
       </CustomizeContainer.SafeView>
     );
   }
